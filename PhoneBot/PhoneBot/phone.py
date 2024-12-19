594892eecc339531d8ec4f336e85eb7390087a41
Ridwan Amokun <70006752+MITXy@users.noreply.github.com>
2024-12-19 16:57:33 +0100

    Add files via upload

---
+++ PhoneBot/PhoneBot/phone.py
@@ -0,0 +1,19 @@
+
+NUMBER = "#############"
+"""
+The default phone number to be processed.
+
+This number is used as a placeholder or default value in the application. 
+It should be replaced with a valid phone number when initializing the PhoneNumber class.
+"""
+
+API_KEY = "######################"
+"""
+The API key for accessing the OpenCage Geocoder service.
+
+This key is required for making requests to the OpenCage Geocoder API to retrieve
+geographical coordinates based on location names. Replace this key with a valid API key
+from your OpenCage Geocoder account.
+
+Note: Keep your API key secure and do not expose it publicly.
+"""
